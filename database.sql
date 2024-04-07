0f48986ebf2dc0a9336cbe032ff50851dfdfc1f9
Koban <46762202+Laimusp@users.noreply.github.com>
2024-04-07 21:09:48 +0300

    Обновили database.sql

--- database.sql
+++ database.sql
@@ -1,25 +1,16 @@
-CREATE TABLE "Institutes"(
-    "id" bigserial NOT NULL,
-    "name" VARCHAR(255) NOT NULL,
-    "short_name" VARCHAR(50) NOT NULL
+CREATE TABLE institutes (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    short_name VARCHAR(50)
 );
-ALTER TABLE
-    "Institutes" ADD PRIMARY KEY("id");
-CREATE TABLE "Users"(
-    "id" BIGINT NOT NULL,
-    "institute_id" BIGINT NOT NULL,
-    "full_name" VARCHAR(255) NOT NULL,
-    "age" BIGINT NOT NULL,
-    "course" BIGINT NOT NULL,
-    "email" VARCHAR(255) NOT NULL,
-    "nickname" VARCHAR(255) NOT NULL,
-    "password_hash" VARCHAR(255) NOT NULL
+
+CREATE TABLE users (
+    id BIGINT PRIMARY KEY,
+    institute_id BIGINT REFERENCES institutes(id),
+    full_name VARCHAR(255),
+    age BIGINT,
+    course BIGINT,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    nickname VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
 );
-ALTER TABLE
-    "Users" ADD PRIMARY KEY("id");
-ALTER TABLE
-    "Users" ADD CONSTRAINT "users_email_unique" UNIQUE("email");
-ALTER TABLE
-    "Users" ADD CONSTRAINT "users_nickname_unique" UNIQUE("nickname");
-ALTER TABLE
-    "Users" ADD CONSTRAINT "users_institute_id_foreign" FOREIGN KEY("institute_id") REFERENCES "Institutes"("id");
